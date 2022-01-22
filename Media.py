6454e35d70a007cb7cd1cce19d7406ad1e390c79
Ketllen Brinker <87481243+Ketllenbrinker@users.noreply.github.com>
2022-01-22 10:34:03 -0300

    Add files via upload

---
+++ Media.py
@@ -0,0 +1,7 @@
+nota1=float(input("Digite o valor da sua nota:"))
+nota2=float(input("Digite o valor da sua nota:"))
+nota3=float(input("Digite o valor da sua nota:"))
+if nota1 >= 7  and nota2 >= 7 and  nota3 >= 7:
+    print("parabéns você foi aprovado!")
+else:
+    print("Reprovado!")
